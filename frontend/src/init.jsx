171cea0f14e544b29a127071dd1bad08aa8e087b
Shamil Kh <shamilist90@gmail.com>
2022-11-13 20:41:37 +0600

    add slices

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Provider as StoreProvider } from 'react-redux';
 import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 
 import App from './components/App.jsx';
 import resources from './locales/index.js';
+import store from './slices/index.js';
 
 const init = async () => {
   const i18n = i18next.createInstance();
@@ -16,9 +18,11 @@ const init = async () => {
     });
 
   return (
-    <I18nextProvider i18n={i18n}>
-      <App />
-    </I18nextProvider>
+    <StoreProvider store={store}>
+      <I18nextProvider i18n={i18n}>
+        <App />
+      </I18nextProvider>
+    </StoreProvider>
   );
 };
 
