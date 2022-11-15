7ef4929c19a4b8d7386c85ee24dfad3c15882409
Shamil Kh <shamilist90@gmail.com>
2022-11-15 16:43:53 +0600

    add messagesSlice

---
+++ frontend/src/slices/messages.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-param-reassign */
+
+import { createEntityAdapter } from '@reduxjs/toolkit';
+
+import { actions as channelsActions, fetchData } from './channelsSlice.js';
+
+const messagesAdapter = createEntityAdapter();
+const initialState = messagesAdapter.getInitialState();
+
+const messagesSlice = ({
+  name: 'messages',
+  initialState,
+  reducers: {
+    addMessage: messagesAdapter.addOne,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchData.fulfilled, (state, action) => {
+        const { messages } = action.payload;
+        messagesAdapter.addMany(state, messages);
+      })
+      .addCase(channelsActions.removeChannel, (state, action) => {
+        const { id } = action.payload;
+        const ids = Object.values(state.entities)
+          .filter((message) => message.channelId === id)
+          .map((message) => message.id);
+        messagesAdapter.removeMany(state, ids);
+      });
+  },
+});
+
+export const { actions } = messagesSlice;
+export default messagesSlice.reducer;
+export const selectors = messagesAdapter.getSelectors((state) => state.messages);
