08dff312b7c5f5c0e2eebdc46be0dc8cd39a0ab3
Shamil Kh <shamilist90@gmail.com>
2022-11-18 15:57:00 +0600

    fix init

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -6,8 +6,11 @@ import { I18nextProvider, initReactI18next } from 'react-i18next';
 import App from './components/App.jsx';
 import resources from './locales/index.js';
 import store from './slices/index.js';
+import ApiProvider from './contexts/ApiProvider.jsx';
+import createAPI from './context/createAPI.js';
 
-const init = async () => {
+const init = async (socket) => {
+  const api = createAPI(socket);
   const i18n = i18next.createInstance();
 
   await i18n
@@ -19,9 +22,11 @@ const init = async () => {
 
   return (
     <StoreProvider store={store}>
-      <I18nextProvider i18n={i18n}>
-        <App />
-      </I18nextProvider>
+      <ApiProvider api={api}>
+        <I18nextProvider i18n={i18n}>
+          <App />
+        </I18nextProvider>
+      </ApiProvider>
     </StoreProvider>
   );
 };
