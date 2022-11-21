36191d9abf2cace88c0b5b1f8f0691ea92f64bdf
Shamil Kh <shamilist90@gmail.com>
2022-11-21 15:36:30 +0600

    add rollbar

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -3,11 +3,19 @@ import { Provider as StoreProvider } from 'react-redux';
 import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import leoProfanity from 'leo-profanity';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import App from './components/App.jsx';
 import resources from './locales/index.js';
 import store from './slices/index.js';
 import ApiProvider from './contexts/ApiProvider.jsx';
 import createAPI from './contexts/api.js';
+
+const rollbarConfig = {
+  accessToken: process.env.ACCESSTOKEN,
+  environment: 'production',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+};
 
 const init = async (socket) => {
   const api = createAPI(socket);
@@ -25,13 +33,17 @@ const init = async (socket) => {
   leoProfanity.add(leoProfanity.getDictionary('eng'));
 
   return (
-    <StoreProvider store={store}>
-      <ApiProvider api={api}>
-        <I18nextProvider i18n={i18n}>
-          <App />
-        </I18nextProvider>
-      </ApiProvider>
-    </StoreProvider>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <StoreProvider store={store}>
+          <ApiProvider api={api}>
+            <I18nextProvider i18n={i18n}>
+              <App />
+            </I18nextProvider>
+          </ApiProvider>
+        </StoreProvider>
+      </ErrorBoundary>
+    </RollbarProvider>
   );
 };
 
