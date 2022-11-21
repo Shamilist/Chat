9eaf5124a3364e94486a96845e9bc2b62fb5a2e9
Shamil Kh <shamilist90@gmail.com>
2022-11-21 17:11:44 +0600

    fix selectors

--- frontend/src/slices/selectors.js
+++ frontend/src/slices/selectors.js
@@ -6,8 +6,8 @@ import { selectors as messagesSelectors } from './messages.js';
 export const getAllChannels = (state) => channelsSelectors.selectAll(state);
 export const getCurrentChannelId = (state) => state.channels.getCurrentChannelId;
 export const getCurrentChannel = (state) => {
-  const currentChanelId = getCurrentChannelId(state);
-  return channelsSelectors.selectById(state, currentChanelId);
+  const currentChannelId = getCurrentChannelId(state);
+  return channelsSelectors.selectById(state, currentChannelId);
 };
 export const isDataFetching = (state) => state.channels.isFetching;
 
