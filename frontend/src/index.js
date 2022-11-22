a50e4e5ece51e9132ee30994f034e28ad0778d4d
Shamil Kh <shamilist90@gmail.com>
2022-11-22 14:45:06 +0600

    fix toastify style

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { io } from 'socket.io-client';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 import init from './init.jsx';
 
