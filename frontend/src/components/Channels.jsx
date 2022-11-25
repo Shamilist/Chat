a242b41530ff330b962bead048aa4fb32e6dd02d
Shamil Kh <shamilist90@gmail.com>
2022-11-25 09:00:47 +0300

    add files in public

--- frontend/src/components/Channels.jsx
+++ frontend/src/components/Channels.jsx
@@ -82,7 +82,7 @@ const Channels = () => {
       </Nav>
     );
   };
-
+  console.log('currId', currentChannelId);
   return (
     <Col className="col-4 col-md-2 border-end pt-5 px-0 bg-light">
       <div className="d-flex justify-content-between mb-2 ps-4 pe-2">
