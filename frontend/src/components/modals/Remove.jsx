bed33a40ba0c94b890c66909e41be82a33f3091a
Shamil Kh <shamilist90@gmail.com>
2022-11-28 10:37:09 +0300

    fix channel redirect after removing

--- frontend/src/components/modals/Remove.jsx
+++ frontend/src/components/modals/Remove.jsx
@@ -6,6 +6,7 @@ import { toast } from 'react-toastify';
 import { useChat } from '../../hooks/index.js';
 import { getItemId } from '../../slices/selectors.js';
 import { actions as modalsActions } from '../../slices/modals.js';
+import { actions as channelsActions } from '../../slices/channels.js';
 
 const Remove = () => {
   const dispatch = useDispatch();
@@ -16,6 +17,7 @@ const Remove = () => {
   const handleRemove = (id) => {
     toast.success(t('modalRemove.success'));
     chat.removeChannel(id);
+    dispatch(channelsActions.setCurrentChannelId(1));
     dispatch(modalsActions.hideModal());
   };
 
